9158aaf4fbeb558021c8d7863b05dfa3301e4eca
Hassan Aguezoum <haguezoum@gmail.com>
2024-07-19 19:56:23 +0000

    start creating folder and files

--- index.js
+++ index.js
@@ -1,55 +1,87 @@
 #!/usr/bin/env node
-const { Console } = require('node:console');
 const fs = require('node:fs');
 const readline = require('node:readline');
 const createRouterFile = require('./Router.js');
+let router = false;
+
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let router = false;
 
-function createFolder() {
-  rl.question('Enter the folder name: ', (folderName) => {
-    if(fs.access(folderName, fs.constants.F_OK, (err) => 
-    {
-      if (err === 'EEXIST') 
-      {
-        console.log('X : '+ err);
-        createFolder();
-      } 
-      // else 
-      // {
-        fs.mkdir(folderName, { recursive: true }, (err) => {
-          if (err) throw err;
-          {
-            createSubfolderStructur(folderName);
-          }
-          console.log('Folder created successfully!');
-          
-          rl.question('Do you want to create a router file? (yes/no): ', (answer) => {
-            if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-              router = true;
-              createRouterFile(folderName);
-              rl.close();
-            } else {
-              rl.close();
-            }
-          });
-        });
-      // }
-    }));
-  }); 
-
-  
-  
+function askQuestion(question) {
+  return new Promise((resolve, reject) => {
+    rl.question(question, (answer) => {
+      if (validInput(answer)) {
+        resolve(answer);
+      } else {
+        reject(new Error("Invalid input"));
+      }
+    });
+  });
 }
 
-createFolder();
+function createFolder(folderName, createRouterFile)
+{
+  if(fs.access(folderName, fs.constants.F_OK, (err) => 
+  {
+    if (err === 'EEXIST') 
+    {
+      console.log('X : '+ err);
+      createFolder();
+    } 
+    fs.mkdir(folderName, { recursive: true }, (err) => {
+      if (err){
+        console.error(err.message);
+      }
+      else
+      {
+        createSubfolderStructur(folderName, createRouterFile);
+      }
+      console.log('Folder created successfully!');
+      
+      
+    });
+  }));
 
-function createSubfolderStructur(parentFolder) {
+}
+
+async function __init__() {
+  try
+  {
+    const validAnwsers = ["yes", "y", "no", "n", "la", "oho", "nn", "ok"];
+
+    const folderName = await askQuestion("Enter the folder name: ");
+    let createRouterFile = await askQuestion("Do you want to start the Router file ? (yes / no) : ");
+    if(!validAnwsers.includes(createRouterFile.toLowerCase()))
+    {
+      console.error("only Yes(y) or No(n) !");
+      createRouterFile = await askQuestion("Do you want to start the Router file ? (yes / no) : ");
+    }
+    else
+      createFolder(folderName,true);
+    rl.close();
+  } 
+  catch (error)
+  {
+    console.error(error)
+  }
+}
+
+function validInput(folderName)
+{
+  if (/^[^0-9][a-zA-Z]{1,}$/.test(folderName))
+    return true;
+  else
+    return false;
+}
+
+
+__init__();
+
+function createSubfolderStructur(parentFolder,router ) {
   try {
     fs.mkdirSync(parentFolder+"/backend");
     fs.mkdirSync(parentFolder+"/backend/"+parentFolder);
@@ -68,9 +100,10 @@ function createSubfolderStructur(parentFolder) {
     fs.writeFileSync(parentFolder+"/frontend/src/components/NavBar.js", "");
     fs.mkdirSync(parentFolder+"/frontend/src/pages");
     fs.mkdirSync(parentFolder+"/frontend/src/services");
-    fs.writeFileSync(parentFolder+"/frontend/src/services/App.js", "");
-    fs.writeFileSync(parentFolder+"/frontend/src/services/index.js", "");
-    fs.writeFileSync(parentFolder+"/frontend/src/services/router.js", "");
+    fs.writeFileSync(parentFolder+"/frontend/src/App.js", "");
+    fs.writeFileSync(parentFolder+"/frontend/src/index.js", "");
+      if(router)
+        fs.writeFileSync(parentFolder+"/frontend/src/router.js", "");
   } catch (error) {
       console.log(error);
   }

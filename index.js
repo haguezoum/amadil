7f6174c787ccd137416a232321f3d830be24b460
Hassan Aguezoum <haguezoum@gmail.com>
2024-08-14 17:50:47 +0000

    fix the make files and dir to asaynchnose methode

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
-const fs = require('node:fs');
+// const fs = require('fs');
+const fs = require('node:fs').promises;
 const readline = require('node:readline');
-const path = require('node:path');
+// const path = require('node:path');
+// if(!fsPromises)
+// {
+//   console.log('fs not found');
+//   return;
+// }else{
+//   console.log('fs found');
+// }
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,7 +18,7 @@ const rl = readline.createInterface({
 
 function askQuestion(question) {
   return new Promise((resolve, reject) => {
-    rl.question(question, (answer) => {
+    rl.question(`\x1b[36m${question}\x1b[0m`, (answer) => {
       if (validFolderName(answer)) {
         resolve(answer);
       } else {
@@ -20,23 +28,22 @@ function askQuestion(question) {
   });
 }
 
-function createFolder(folderName, createRouterFile)
+async function createFolder(folderName, createRouterFile)
 {
-  if(fs.access(folderName, fs.constants.F_OK, (err) => 
-  {
-    if (!err) 
-    {
-      console.log(`Folder \x1b[31m${folderName}\x1b[0m already exists.`);
-      return;
-    } 
-    fs.mkdir(folderName, { recursive: true }, (err) => {
-      if (err){
-        console.error(err.message);
-      }
+  try {
+    await fs.access(folderName, fs.constants.F_OK);
+    console.log(`Folder \x1b[31m${folderName}\x1b[0m already exists.`);
+  } catch (err) {
+      try{
+        await fs.mkdir(folderName, { recursive: true });
         createSubfolderStructur(folderName, createRouterFile);
         console.log('Folder created successfully!');
-    });
-  }));
+      }
+      catch(error)
+      {
+        console.error(error.message);
+      }
+  }
 }
 
 async function __init__() {
@@ -65,29 +72,30 @@ function validFolderName(folderName)
 
 __init__();
 
-function createSubfolderStructur(parentFolder, router) {
+async function createSubfolderStructur(parentFolder, router) {
   try {
-    fs.mkdirSync(parentFolder+"/backend");
-    fs.mkdirSync(`${parentFolder}/backend/${parentFolder}`);
-    fs.mkdirSync(`${parentFolder}/backend/apps`);
-    fs.mkdirSync(`${parentFolder}/backend/static`);
-    fs.mkdirSync(`${parentFolder}/backend/templates`);
-    fs.writeFileSync(`${parentFolder}/backend/manage.py`, "");
-    fs.writeFileSync(`${parentFolder}/backend/requirements.txt`, "");
+    // await fs.mkdir("dldl");
+    await fs.mkdir(parentFolder+"/backend");
+    await fs.mkdir(`${parentFolder}/backend/${parentFolder}`);
+    await fs.mkdir(`${parentFolder}/backend/apps`);
+    await fs.mkdir(`${parentFolder}/backend/static`);
+    await fs.mkdir(`${parentFolder}/backend/templates`);
+    await fs.writeFile(`${parentFolder}/backend/manage.py`, "");
+    await fs.writeFile(`${parentFolder}/backend/requirements.txt`, "");
 
-    fs.mkdirSync(`${parentFolder}/frontend`);
-    fs.mkdirSync(`${parentFolder}/frontend/public`);
-    fs.writeFileSync(`${parentFolder}/frontend/public/index.html`, "");
-    fs.mkdirSync(`${parentFolder}/frontend/src`);
-    fs.mkdirSync(`${parentFolder}/frontend/src/assets`);
-    fs.mkdirSync(`${parentFolder}/frontend/src/components`);
-    fs.writeFileSync(`${parentFolder}/frontend/src/components/NavBar.js`, "");
-    fs.mkdirSync(`${parentFolder}/frontend/src/templates`);
-    fs.mkdirSync(`${parentFolder}/frontend/src/pages`);
-    fs.mkdirSync(`${parentFolder}/frontend/src/services`);
-    fs.writeFileSync(`${parentFolder}/frontend/src/App.js`, "");
-    fs.writeFileSync(`${parentFolder}/frontend/src/index.js`, "");
-      router ? fs.writeFileSync(`${parentFolder}/frontend/src/router.js`, ""):false;
+    fs.mkdir(`${parentFolder}/frontend`);
+    fs.mkdir(`${parentFolder}/frontend/public`);
+    fs.writeFile(`${parentFolder}/frontend/public/index.html`, "");
+    fs.mkdir(`${parentFolder}/frontend/src`);
+    fs.mkdir(`${parentFolder}/frontend/src/assets`);
+    fs.mkdir(`${parentFolder}/frontend/src/components`);
+    fs.writeFile(`${parentFolder}/frontend/src/components/NavBar.js`, "");
+    fs.mkdir(`${parentFolder}/frontend/src/templates`);
+    fs.mkdir(`${parentFolder}/frontend/src/pages`);
+    fs.mkdir(`${parentFolder}/frontend/src/services`);
+    fs.writeFile(`${parentFolder}/frontend/src/App.js`, "");
+    fs.writeFile(`${parentFolder}/frontend/src/index.js`, "");
+      router ? fs.writeFile(`${parentFolder}/frontend/src/router.js`, ""):false;
   } catch (error) {
       console.log(error);
   }

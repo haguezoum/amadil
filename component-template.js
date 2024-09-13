00e49508cc462f3bdd4bab07cf18d1d6c2646a14
Hassan aguezoum <haguezoum@gmail.com>
2024-09-13 18:07:21 +0100

    insert html inside js

--- component-template.js
+++ component-template.js
@@ -1,20 +1,41 @@
 class __COMPONENT_NAME__ extends HTMLElement {
-    constructor() {
-      super();
-      const shadowRoot = this.attachShadow({ mode: 'open' });
-      this.render(shadowRoot);
-    }
-    
-    async render(shadowRoot){
-      try{
-        const response = await fetch('../templates/__COMPONENT_TAG__.html');
-        const content = await response.text();
-        const template = document.createElement('template');
-        template.innerHTML = content;
-        shadowRoot.appendChild(template.content.cloneNode(true));
-      }catch(err){
-      console.error(err);
-      }
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  async connectedCallback() {
+    console.log('Nav bar connected');
+    const templateContent = await this.fetchTemplate();
+    this.shadowRoot.innerHTML = templateContent;
+  }
+  
+  async disconnectedCallback() {
+    console.log('Nav bar disconnected');
+  }
+
+  static get observedAttributes() {
+    return[/* array of attribute names to monitor for changes */];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // called when one of attributes listed above is modified
+  }
+
+  async fetchTemplate() {
+  
+    return(
+    `
+    <style>
+        @import "../styles/__COMPONENT_TAG__.css";
+    </style>
+    <h1>
+      this is __COMPONENT_NAME__ component
+    </h1>
+    `
+    );
+  }
 }
   customElements.define('__COMPONENT_TAG__', __COMPONENT_NAME__);
+
+export default __COMPONENT_NAME__;
--- componentConstractor.js
+++ componentConstractor.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 const fs = require('node:fs').promises;
 const path = require('path');
-const initFiles =  require('./initFiles.js');
+const {getConfig} = require("./finedr.js")
+// const initFiles =  require('./initFiles.js');
 const componentName = process.argv[2];
 const envPath = path.join(__dirname, '.env');
 let outputDir ;
-
 
 if(!componentName){ 
     console.log('Usage:component <component_name>');
@@ -18,18 +18,11 @@ if(!componentName.includes('-')){
 }
 const componentTag = componentName.toLowerCase();
 const componentClassName = componentName.charAt(0).toUpperCase() + componentName.slice(1);
-const htmlTemplatePath = path.join(__dirname, 'component-template.html');
 const jsTemplatePath   = path.join(__dirname, 'component-template.js');
-  
-const parentDir = async () => {
-  const envData = await fs.readFile(envPath, 'utf8');
-  return envData.split("=")[1].trim();
-};
 
 (async () => {
   try {
-    outputDir = await parentDir();
-    console.log(outputDir);
+    outputDir = await getConfig().paths.project;
     outputDir =  path.join(outputDir, 'frontend', 'src');
     fs.access(outputDir, fs.constants.F_OK, (err) => {
       if (err) {
@@ -37,9 +30,7 @@ const parentDir = async () => {
         return;
       }
     });
-    const htmlTemplate = await fs.readFile(htmlTemplatePath, 'utf8');
-    const processedHtmlTemplate = htmlTemplate.replace(/__COMPONENT_NAME__/g, componentClassName);
-    await fs.writeFile(path.join(outputDir, 'templates', `${componentName}.html`), processedHtmlTemplate);
+    await fs.writeFile(path.join(outputDir, 'assets/style', `${componentName}.css`), `/****** ${componentName}.css ******/`);
     const jsTemplate = await fs.readFile(jsTemplatePath, 'utf8');
     const processedJsTemplate = jsTemplate.replace(/__COMPONENT_NAME__/g, componentClassName.replace(/-/g, ''))
       .replace(/__COMPONENT_TAG__/g, componentTag);

9f2d718adb994de55bcfa84cfc4eb68d404b4e93
Hassan Aguezoum <haguezoum@gmail.com>
2024-09-20 17:41:16 +0000

    add page generator

--- pageConstractor.js
+++ pageConstractor.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 const path = require('node:path');
+const {getConfig} = require("./finedr.js");
+const { stdout } = require('node:process');
 
-const pagetName = process.argv[2];
+const   projectPath = getConfig().paths.project;
+let     pageName = process.argv[2] ? process.argv[2].trim().toUpperCase() : (console.error("Usage: page <page-name>"), process.exit(1));
+const   pagePath = path.join(projectPath, 'frontend/src/pages');
+const   jsTemplatePath = path.join(__dirname, 'component-template.js');
 
-
-const pagePath = path.join(process.cwd(), 'frontend', 'src', 'pages', `${pagetName}.js`);
+(async ()=>{
+    try {
+        try {
+            await fs.access(pagePath);
+        } catch {
+            await fs.mkdir(pagePath, { recursive: true });
+        }
+        const jsTemplate = await fs.readFile(jsTemplatePath, 'utf8');
+        const processedJsTemplate = jsTemplate.replace(/__COMPONENT_NAME__/g, pageName).replace(/__COMPONENT_TAG__/g, `${pageName.toLowerCase()}-page`);
+        await fs.writeFile(`${pagePath}/${pageName.toLowerCase()}.js`, processedJsTemplate);
+        console.info(`Page ${pageName} created successfully!`)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+})()
